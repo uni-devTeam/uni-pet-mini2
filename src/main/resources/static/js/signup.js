ccd950976e6014a4537b7b4b6eaf78bea1460e87
hanseungwan1 <seungwanhan542@gmail.com>
2023-10-17 10:24:57 +0900

    feat: 프회원가입 미보유 클릭시 다른 인풋태그 비활성화 작업

--- src/main/resources/static/js/signup.js
+++ src/main/resources/static/js/signup.js
@@ -2,6 +2,7 @@ const alertBtn = document.querySelector(".alert_btn");
 const alertContainer = document.querySelector(".alert");
 const blackBg = document.querySelector(".black_bg");
 
+if(alertBtn !=null && blackBg!= null){
 alertBtn.addEventListener("click", () => {
   blackBg.classList.add("display_none");
   alertContainer.classList.add("display_none");
@@ -11,3 +12,22 @@ blackBg.addEventListener("click", () => {
   blackBg.classList.add("display_none");
   alertContainer.classList.add("display_none");
 });
+}
+
+
+    function toggleInputs() {
+      var havePetTrue = document.getElementById("havePetTrue");
+      var havePetFalse = document.getElementById("havePetFalse");
+      var inputFields = document.querySelectorAll(".disable_toggle");
+
+      for (var i = 0; i < inputFields.length; i++) {
+        inputFields[i].disabled = havePetFalse.checked;
+      }
+    }
+
+    // Add an event listener to the radio buttons
+    document.getElementById("havePetTrue").addEventListener("change", toggleInputs);
+    document.getElementById("havePetFalse").addEventListener("change", toggleInputs);
+
+    // Initial call to set the initial state
+    toggleInputs();
