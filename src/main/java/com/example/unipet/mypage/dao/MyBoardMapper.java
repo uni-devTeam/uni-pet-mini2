8786328c01c23677dee27f1d36da5b00dc9b134e
Minji287 <dpa357@gmail.com>
2023-10-18 09:33:40 +0900

    fix 세션 가져오는 방법 변경

--- src/main/java/com/example/unipet/mypage/dao/MyBoardMapper.java
+++ src/main/java/com/example/unipet/mypage/dao/MyBoardMapper.java
@@ -12,16 +12,16 @@ public interface MyBoardMapper {
     // 나의 나눔 가져오기
     @Select("SELECT board_no, board_id, title, content, posting_date, img_path " +
             "FROM boards " +
-            "WHERE user_id = #{sessionId} " +
+            "WHERE user_id = #{userId} " +
             "ORDER BY board_no DESC")
-    public List<BoardVO> getMyShare(@Param("sessionId") String sessionId);
+    public List<BoardVO> getMyShare(@Param("userId") String userId);
 
     // 나눔 좋아요 가져오기
     @Select("SELECT b.board_no, b.board_id, b.title, b.content, b.posting_date, b.img_path " +
             "FROM boards b " +
             "LEFT JOIN uni_pet.like l " +
             "ON b.board_no = l.board_no " +
-            "WHERE l.user_id = #{sessionId} " +
+            "WHERE l.user_id = #{userId} " +
             "ORDER BY l.board_no DESC")
-    public List<BoardVO> getShareLikes(@Param("sessionId") String sessionId);
+    public List<BoardVO> getShareLikes(@Param("userId") String userId);
 }
