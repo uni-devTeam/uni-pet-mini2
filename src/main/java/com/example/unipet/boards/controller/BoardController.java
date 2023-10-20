20ac5b640099026e289a05fce5de83581c1fffbb
HaInseong <gkdlstjd1020@naver.com>
2023-10-20 18:36:30 +0900

    게ìfeat: 게시판 기본 기능 구현 완료

--- src/main/java/com/example/unipet/boards/controller/BoardController.java
+++ src/main/java/com/example/unipet/boards/controller/BoardController.java
@@ -2,12 +2,11 @@ package com.example.unipet.boards.controller;
 
 import com.example.unipet.boards.dao.BoardMapper;
 import com.example.unipet.boards.domain.BoardDTO;
+import com.example.unipet.boards.domain.CommentDTO;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -18,18 +17,20 @@ public class BoardController {
     @Autowired
     BoardMapper dao;
 
-    @RequestMapping("posting")
-    public String writePost() {
-        return "posting";
-    }
+    @RequestMapping("list{board_id}")
+    public ModelAndView BoardList(@PathVariable("board_id") int board_id) {
+        BoardDTO dto = new BoardDTO();
+        dto.setBoard_id(board_id);
 
-    @RequestMapping("/sbsearch")
-    public ModelAndView search(String title) {
-        List<BoardDTO> list = dao.search(title);
-        //System.out.println(list.size());
+        List<BoardDTO> list = dao.boardId(dto);
         ModelAndView mav = new ModelAndView();
+        mav.addObject("boardId", board_id);
+
         if (list.size() != 0) {
             mav.addObject("list", list);
+//            String userId = (String) request.getSession().getAttribute("userId");
+//            boolean showWriteButton = "admin".equals(userId);
+//            mav.addObject("showWriteButton", showWriteButton);
         } else {
             mav.addObject("msg", "작성된 게시물이 존재하지 않습니다.");
         }
@@ -37,32 +38,20 @@ public class BoardController {
         return mav;
     }
 
-    @RequestMapping("sblist")
-    public ModelAndView shareBoardList() {
-        List<BoardDTO> list = dao.list();
-        ModelAndView mav = new ModelAndView();
-        if (list.size() != 0) {
-            mav.addObject("list", list);
-        } else {
-            mav.addObject("msg", "작성된 게시물이 존재하지 않습니다.");
-        }
-        mav.setViewName("board");
-        return mav;
-    }
 
-    @RequestMapping("sbsave")
+    @RequestMapping("save")
     public ModelAndView insert(BoardDTO save) {
         boolean result = dao.insert(save);
         ModelAndView mav = new ModelAndView();
         if (result) {
-            mav.setViewName("redirect:/board/sblist");
+            mav.setViewName("redirect:/board/list" + save.getBoard_id());
         } else {
             mav.addObject("msg", "글 등록에 실패했습니다.");
         }
         return mav;
     }
 
-    @RequestMapping("sbdelete")
+    @RequestMapping("delete")
     public ModelAndView delete(int board_no) {
         boolean result = dao.delete(board_no);
         ModelAndView mav = new ModelAndView();
@@ -89,15 +78,51 @@ public class BoardController {
         return mav;
     }
 
+    @RequestMapping("/sbsearch")
+    public ModelAndView search(String title) {
+        List<BoardDTO> list = dao.search(title);
+        //System.out.println(list.size());
+        ModelAndView mav = new ModelAndView();
+        if (list.size() != 0) {
+            mav.addObject("list", list);
+        } else {
+            mav.addObject("msg", "작성된 게시물이 존재하지 않습니다.");
+        }
+        mav.setViewName("board");
+        return mav;
+    }
+
+    @RequestMapping("posting")
+    public ModelAndView writePost(@RequestParam("board_id") int boardId) {
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("boardId", boardId);
+        mav.setViewName("posting");
+        return mav;
+    }
+
     @RequestMapping(value = "/content", produces = "application/json; charset=utf-8")
     @ResponseBody
-    public ModelAndView showContent(@RequestParam("board_no") int board_no) {
+    public ModelAndView showContent(@RequestParam("board_no") int board_no, HttpSession session) {
         System.out.println(board_no);
-
         dao.increaseViews(board_no);
-        BoardDTO board = dao.showContent(board_no);
+        BoardDTO board = dao.showContent(board_no); //DTO 반환
+
+        List<CommentDTO> comments = dao.getComments(board_no); //Comment 리스트 가져오기
+
         ModelAndView mav = new ModelAndView();
         mav.addObject("board", board);
+        mav.addObject("boardId", board.getBoard_id());
+
+        mav.addObject("comments", comments);
+
+        String currentUserId = (String) session.getAttribute("userId");
+        System.out.println("로그인 중인 아이디:" + currentUserId);
+        if(currentUserId != null && currentUserId.equals((board.getUser_id()))) {
+            mav.addObject("canEdit", true);
+        } else {
+            mav.addObject("canEdit", false);
+        }
+
         mav.setViewName("content");
         return mav;
     }
@@ -107,4 +132,7 @@ public class BoardController {
     public BoardDTO one(int board_no) {
         return dao.one(board_no);
     }
+
+
+
 }
--- src/main/java/com/example/unipet/boards/dao/BoardMapper.java
+++ src/main/java/com/example/unipet/boards/dao/BoardMapper.java
@@ -1,20 +1,25 @@
 package com.example.unipet.boards.dao;
 
 import com.example.unipet.boards.domain.BoardDTO;
+import com.example.unipet.boards.domain.CommentDTO;
 import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
 @Mapper
 public interface BoardMapper {
-    @Select("select board_no, user_id, board_id, title, content, date_format(posting_date, '%Y-%m-%d') posting_date, views from Boards")
+
+    @Select("select * from boards where board_id = #{board_id} order by posting_date desc")
+    List<BoardDTO> boardId(BoardDTO dto);
+
+    @Select("select board_no, user_id, board_id, title, content, date_format(posting_date, '%Y-%m-%d') posting_date, views from boards")
     public List<BoardDTO> list();
 
     @Insert("insert into boards (user_id, board_id, title, posting_date, content) values (#{user_id}, #{board_id}, #{title}, now(), #{content})")
     public boolean insert(BoardDTO save);
 
-    @Select("select user_id, title, board_no, date_format(posting_date, '%Y.%m.%d. %m:%s') posting_date, content, views from boards where board_no = #{board_no}")
-    public BoardDTO showContent(int board_no); //one
+    @Select("select board_no, user_id, board_id, title, date_format(posting_date, '%Y.%m.%d. %m:%s') posting_date, content, views, like_counting, img_path from boards where board_no = #{board_no}")
+    public BoardDTO showContent(int board_no); //one //DTO 반환
 
     @Update("update boards set views = views + 1 where board_no = #{board_no}")
     void increaseViews(int board_no);
@@ -28,7 +33,17 @@ public interface BoardMapper {
     @Update("update boards set user_id = #{user_id}, content = #{content} where board_no = #{board_no}")
     public boolean update(BoardDTO board);
 
-    @Select("select user_id, title, board_no, date_format(posting_date, '%Y.%m.%d. %m:%s') posting_date, content, views from boards where board_no = #{board_no}")
+    @Select("select user_id, title, board_no, board_id, date_format(posting_date, '%Y.%m.%d. %m:%s') posting_date, content, views from boards where board_no = #{board_no}")
     public BoardDTO one(int board_no);
 
+    //
+//    @Insert("insert into comment (user_id, board_no, comment_id, content) values (#{user_id}, #{board_no}, #{comment_id}, #{content})")
+//    boolean insert(CommentDTO commentDTO);
+//
+//    @Select("SELECT * FROM comment WHERE board_no = #{boardNo}")
+//    List<CommentDTO> selectByBoardNo(int boardNo);
+
+    @Select("select * from comment where board_no = #{board_no}")
+    public List<CommentDTO> getComments(int board_no);
 }
+
