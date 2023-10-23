9bc723e9f9ec603b63b532a9fdfa174f7f054881
hanseungwan1 <seungwanhan542@gmail.com>
2023-10-23 15:25:47 +0900

    fixed untracked files

--- src/main/resources/static/js/apikey.js
+++ src/main/resources/static/js/apikey.js
@@ -1,3 +1,8 @@
 const config = {
-	apikey: "664f746c5373657535347665537077"
+    apikey: "664f746c5373657535347665537077"
+}
+
+const weather_config = {
+    WAPI_KEY:
+        "9eaa9bb68798c27060b339e5c7bfdfa0"
 }
