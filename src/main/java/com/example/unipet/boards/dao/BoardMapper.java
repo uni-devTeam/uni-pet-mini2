9783ac97dc994451207b11d1aa4ea60431623c43
HaInseong <gkdlstjd1020@naver.com>
2023-10-23 14:38:29 +0900

    feat:댓글 기능 완성

--- src/main/java/com/example/unipet/boards/dao/BoardMapper.java
+++ src/main/java/com/example/unipet/boards/dao/BoardMapper.java
@@ -27,23 +27,20 @@ public interface BoardMapper {
     @Select("select board_no, user_id, board_id, title, content, date_format(posting_date, '%Y-%m-%d') posting_date, views from boards where title like concat('%', #{title},'%')")
     public List<BoardDTO> search(String title);
 
-    @Delete("delete from boards where board_no = #{board_no}")//나중에 user_id = #{user_id}
+    @Delete("delete from boards where board_no = #{board_no}")//세션 로그인 구현 전 임의 저장한 id 사용 중이라 나중에 user_id = #{user_id} 처리해야 함.
     public boolean delete(int board_no);
 
-    @Update("update boards set user_id = #{user_id}, content = #{content} where board_no = #{board_no}")
+    @Update("update boards set content = #{content} where board_no = #{board_no}")
     public boolean update(BoardDTO board);
 
     @Select("select user_id, title, board_no, board_id, date_format(posting_date, '%Y.%m.%d. %m:%s') posting_date, content, views from boards where board_no = #{board_no}")
-    public BoardDTO one(int board_no);
-
-    //
-//    @Insert("insert into comment (user_id, board_no, comment_id, content) values (#{user_id}, #{board_no}, #{comment_id}, #{content})")
-//    boolean insert(CommentDTO commentDTO);
-//
-//    @Select("SELECT * FROM comment WHERE board_no = #{boardNo}")
-//    List<CommentDTO> selectByBoardNo(int boardNo);
+    public BoardDTO getBoard(int board_no);
 
     @Select("select * from comment where board_no = #{board_no}")
     public List<CommentDTO> getComments(int board_no);
+
+    @Insert("insert into comment (board_no, user_id, comment_date, content) values (#{board_no}, #{user_id}, now(), #{content})")
+    public boolean comment(CommentDTO save);
+
 }
 
