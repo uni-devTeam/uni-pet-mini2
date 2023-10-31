f1f42cbad3c92d6409f931ed4089de2ac3edf6f5
hanseungwan1 <seungwanhan542@gmail.com>
2023-10-31 17:15:32 +0900

    init: JPA INIT

--- src/main/java/com/example/unipet/UnipetApplication.java
+++ src/main/java/com/example/unipet/UnipetApplication.java
@@ -3,7 +3,9 @@ package com.example.unipet;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
@@ -11,7 +13,8 @@ import org.springframework.context.annotation.ComponentScan;
 @MapperScan(value = {"com.example.unipet.mypage.dao", "com.example.unipet.signup.dao",
         "com.example.unipet.login.dao", "com.example.unipet.main.dao",
         "com.example.unipet.boards.dao", "com.example.unipet.list.dao"})
-
+@EnableJpaRepositories(basePackages = {"springjpa.exam.repository"})
+@EntityScan(basePackages = {"springjpa.exam.entity"})
 public class UnipetApplication {
     public static void main(String[] args) {
         SpringApplication.run(UnipetApplication.class, args);
