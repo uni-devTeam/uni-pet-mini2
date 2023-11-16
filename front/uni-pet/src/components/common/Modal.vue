e4b0152015c7e6dd44cf77bef3247fa3a423fc42
hanseungwan1 <seungwanhan542@gmail.com>
2023-11-16 09:29:19 +0900

    style: 로그인 페이지 스타일링 완료

---
+++ front/uni-pet/src/components/common/Modal.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="black_bg" th:if="${message}"></div>
+  <div class="alert" th:if="${message}">
+    <p th:text="${message}"></p>
+    <button class="alert_btn">확인</button>
+  </div>
+</template>
+
+<script setup>
+import useModal from "../../assets/js/modal";
+import { onMounted } from "vue";
+onMounted(() => {
+  useModal();
+});
+</script>
+
+<style scoped>
+@import "../../assets/css/modal.css";
+</style>
--- front/uni-pet/src/router/index.js
+++ front/uni-pet/src/router/index.js
@@ -1,19 +1,21 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
-import MainPage from '@/pages/MainPage.vue'
-import Mypage from '@/pages/MyPage.vue'
+import MainPage from "@/pages/MainPage.vue";
+import Mypage from "@/pages/MyPage.vue";
+import LoginPage from "@/pages/LoginPage.vue";
 
 const index = createRouter({
-    history: createWebHistory(),
-    routes : [
-        { path: '/', component: MainPage },
-        { path: '/mypage', component: Mypage },
-        { path: '/myprofile', component: Mypage },
-        { path: '/mypet', component: Mypage },
-        { path: '/mywriting', component: Mypage },
-        { path: '/myshare', component: Mypage },
-        { path: '/mysharelikes', component: Mypage },
-    ]
-})
+  history: createWebHistory(),
+  routes: [
+    { path: "/", component: MainPage },
+    { path: "/login", component: LoginPage },
+    { path: "/mypage", component: Mypage },
+    { path: "/myprofile", component: Mypage },
+    { path: "/mypet", component: Mypage },
+    { path: "/mywriting", component: Mypage },
+    { path: "/myshare", component: Mypage },
+    { path: "/mysharelikes", component: Mypage },
+  ],
+});
 
 export default index;
